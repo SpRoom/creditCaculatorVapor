657afe91bb7984ea08278febd8b7d6905b7d7bb9
spectatorNan <spectatornan@gmail.com>
2018-10-30 14:31:44 +0800

    Update Sources/App/Routers/AccountRouter.swift

--- Sources/App/Routers/AccountRouter.swift
+++ Sources/App/Routers/AccountRouter.swift
@@ -13,14 +13,34 @@ extension AccountController : RouteCollection {
     
     func boot(router: Router) throws {
         
+        /**
+         *  @apidoc account
+         *  @apiVersion 1.0
+         *  @apiBaseURL http://localhost:8080
+         *
+         */
+        
+        
+        
         let apiV1 = router.grouped("api","v1").grouped(AuthMiddleware())
         
         let accountV1 = apiV1.grouped("account")
+        
         
         accountV1.post(NameContainer.self, at: "addAccountType", use: addAccountType)
         accountV1.post("accountTypes", use: accountTypes)
         accountV1.post(AccountContainer.self, at: "addAccount", use: addAccount)
         accountV1.post(EditAccountContainer.self, at: "editAccount", use: editAccount)
+        /**
+         *  @api post /api/v1/account/account 获取账户信息
+         *  @apiGroup account
+         * 
+         *  @apiParam id int 对应账户ID
+         *
+         *  @apiSuccess 1000 OK
+         *  @apiExample json
+         *  { "id" : 1 }
+         */
         accountV1.post(IDContainer.self, at: "account", use: account)
     }
 }
